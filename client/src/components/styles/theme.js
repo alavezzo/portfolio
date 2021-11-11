993e8652e83113de6e81447a6448d6fe1fe35595
bowdenshot <anthonyelavezzo@gmail.com>
2021-11-10 19:39:37 -0800

    initial color mode bug

--- client/src/components/styles/theme.js
+++ client/src/components/styles/theme.js
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
+    config: {
+        initialColorMode: 'light',
+        useSystemColorMode: false
+    },
     colors: {
         darkPurple: '#351431',
         rossoCorso: '#21A0A0',
@@ -8,9 +12,5 @@ export const theme = extendTheme({
         midnightGreen: '#02394A',
         platinum: '#DFE0DC',
         end: '#02444A'
-    },
-    config: {
-        initialColorMode: 'light',
-        useSystemColorMode: false
     }
 })
